3533201526c7edc47a3b438795118d2529efb22c
Murial Anindo <murial.anindo@lonett.com>
2023-06-09 19:18:19 -0400

    adding secion page

---
+++ components/MainSection.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function MainSection() {
+  return (
+    <section>
+      <div>
+        <p>
+          Back in 2018, I decided to get into Software Development, Fast Foward
+          to today -
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default MainSection;
