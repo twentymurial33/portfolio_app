1d760509e001eb623ac4c992dbfebb3be3c8951a
Murial Anindo <murial.anindo@lonett.com>
2023-07-14 16:44:14 -0400

    added more experiences

--- components/About.js
+++ components/About.js
@@ -4,20 +4,25 @@ function About() {
   return (
     <section>
       <div>
-        <p>
+        <Text>
           Back in 2018, I decided to disrupt my life and dive into learning
-          programming. I enrolled to a coding bootcamp and there my journey
-          started themes and tumbled head first into the rabbit hole of coding
-          and web development. Fast-forward to today, and had the privilege of
-          building software for an advertising agency, a start-up, a student-led
-          design studio, and a huge corporation. When Im not at the computer, Im
-          usually rock climbing, hanging out with my wife and two cats, or
-          running around Hyrule searching for Korok seeds
-        </p>
+          programming.
+          <br />I enrolled to a coding bootcamp and there my journey started
+          themes <br />
+          and tumbled head first into the rabbit hole of coding and web
+          development.
+          <br /> Fast-forward to today, and had the privilege of building
+          software for an advertising agency,
+          <br />a start-up, a student-led design studio, and a huge corporation.
+        </Text>
       </div>
     </section>
   );
 }
 
-const Text = styled.div``;
+const Text = styled.p`
+  font-size: 20px;
+  color: white;
+  text-align: center;
+`;
 export default About;
--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,31 @@
-import React from 'react'
+//import BottomNavigation from "@mui/material/BottomNavigation";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import styled from "styled-components";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <>
+      <BottomNavigation>
+        <LinkedInIcon />
+        <GitHubIcon />
+        <div>
+          <p>
+            Loosely designed in Figma and coded in Visual Studio Code by yours
+            truly. Built with Next.js and Tailwind CSS, deployed with Vercel.
+            All text is set in the Inter typeface.
+          </p>
+        </div>
+      </BottomNavigation>
+    </>
+  );
 }
 
-export default Footer
+const BottomNavigation = styled.div`
+  height: 100px;
+  color: white;
+  padding: 20px 20px;
+  margin: 40px 40px;
+`;
+
+export default Footer;
